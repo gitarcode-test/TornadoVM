64ef872535b8e1ab9404b0da7b9d4187c800f653
Gitar <noreply@gitar.co>
2024-09-21 09:03:06 +0000

    Gig completed

--- tornado-unittests/src/main/java/uk/ac/manchester/tornado/unittests/api/TestBuildFromByteBuffers.java
+++ tornado-unittests/src/main/java/uk/ac/manchester/tornado/unittests/api/TestBuildFromByteBuffers.java
@@ -17,7 +17,23 @@
  */
 package uk.ac.manchester.tornado.unittests.api;
 
-import static org.junit.Assert.assertEquals;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.DoubleBuffer;
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+import java.nio.LongBuffer;
+import java.nio.ShortBuffer;
+import org.junit.jupiter.api.Test;
+import uk.ac.manchester.tornado.api.types.arrays.ByteArray;
+import uk.ac.manchester.tornado.api.types.arrays.CharArray;
+import uk.ac.manchester.tornado.api.types.arrays.DoubleArray;
+import uk.ac.manchester.tornado.api.types.arrays.FloatArray;
+import uk.ac.manchester.tornado.api.types.arrays.IntArray;
+import uk.ac.manchester.tornado.api.types.arrays.LongArray;
+import uk.ac.manchester.tornado.api.types.arrays.ShortArray;
+import uk.ac.manchester.tornado.unittests.common.TornadoTestBase;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
@@ -26,7 +42,115 @@ import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
 import java.nio.LongBuffer;
 import java.nio.ShortBuffer;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import uk.ac.manchester.tornado.api.types.arrays.ByteArray;
+import uk.ac.manchester.tornado.api.types.arrays.CharArray;
+import uk.ac.manchester.tornado.api.types.arrays.DoubleArray;
+import uk.ac.manchester.tornado.api.types.arrays.FloatArray;
+import uk.ac.manchester.tornado.api.types.arrays.IntArray;
+import uk.ac.manchester.tornado.api.types.arrays.LongArray;
+import uk.ac.manchester.tornado.api.types.arrays.ShortArray;
+import uk.ac.manchester.tornado.unittests.common.TornadoTestBase;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.DoubleBuffer;
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+import java.nio.LongBuffer;
+import java.nio.ShortBuffer;
+import org.junit.jupiter.api.Test;
+import uk.ac.manchester.tornado.api.types.arrays.ByteArray;
+import uk.ac.manchester.tornado.api.types.arrays.CharArray;
+import uk.ac.manchester.tornado.api.types.arrays.DoubleArray;
+import uk.ac.manchester.tornado.api.types.arrays.FloatArray;
+import uk.ac.manchester.tornado.api.types.arrays.IntArray;
+import uk.ac.manchester.tornado.api.types.arrays.LongArray;
+import uk.ac.manchester.tornado.api.types.arrays.ShortArray;
+import uk.ac.manchester.tornado.unittests.common.TornadoTestBase;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.DoubleBuffer;
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+import java.nio.LongBuffer;
+import java.nio.ShortBuffer;
+import org.junit.jupiter.api.Test;
+import uk.ac.manchester.tornado.api.types.arrays.ByteArray;
+import uk.ac.manchester.tornado.api.types.arrays.CharArray;
+import uk.ac.manchester.tornado.api.types.arrays.DoubleArray;
+import uk.ac.manchester.tornado.api.types.arrays.FloatArray;
+import uk.ac.manchester.tornado.api.types.arrays.IntArray;
+import uk.ac.manchester.tornado.api.types.arrays.LongArray;
+import uk.ac.manchester.tornado.api.types.arrays.ShortArray;
+import uk.ac.manchester.tornado.unittests.common.TornadoTestBase;
+import static org.hamcrest.number.IsCloseTo.closeTo;
+
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.DoubleBuffer;
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+import java.nio.LongBuffer;
+import java.nio.ShortBuffer;
+import org.junit.jupiter.api.Test;
+import uk.ac.manchester.tornado.api.types.arrays.ByteArray;
+import uk.ac.manchester.tornado.api.types.arrays.CharArray;
+import uk.ac.manchester.tornado.api.types.arrays.DoubleArray;
+import uk.ac.manchester.tornado.api.types.arrays.FloatArray;
+import uk.ac.manchester.tornado.api.types.arrays.IntArray;
+import uk.ac.manchester.tornado.api.types.arrays.LongArray;
+import uk.ac.manchester.tornado.api.types.arrays.ShortArray;
+import uk.ac.manchester.tornado.unittests.common.TornadoTestBase;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.DoubleBuffer;
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+import java.nio.LongBuffer;
+import java.nio.ShortBuffer;
+import org.junit.jupiter.api.Test;
+import uk.ac.manchester.tornado.api.types.arrays.ByteArray;
+import uk.ac.manchester.tornado.api.types.arrays.CharArray;
+import uk.ac.manchester.tornado.api.types.arrays.DoubleArray;
+import uk.ac.manchester.tornado.api.types.arrays.FloatArray;
+import uk.ac.manchester.tornado.api.types.arrays.IntArray;
+import uk.ac.manchester.tornado.api.types.arrays.LongArray;
+import uk.ac.manchester.tornado.api.types.arrays.ShortArray;
+import uk.ac.manchester.tornado.unittests.common.TornadoTestBase;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.DoubleBuffer;
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+import java.nio.LongBuffer;
+import java.nio.ShortBuffer;
+import org.junit.jupiter.api.Test;
+import uk.ac.manchester.tornado.api.types.arrays.ByteArray;
+import uk.ac.manchester.tornado.api.types.arrays.CharArray;
+import uk.ac.manchester.tornado.api.types.arrays.DoubleArray;
+import uk.ac.manchester.tornado.api.types.arrays.FloatArray;
+import uk.ac.manchester.tornado.api.types.arrays.IntArray;
+import uk.ac.manchester.tornado.api.types.arrays.LongArray;
+import uk.ac.manchester.tornado.api.types.arrays.ShortArray;
+import uk.ac.manchester.tornado.unittests.common.TornadoTestBase;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.DoubleBuffer;
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+import java.nio.LongBuffer;
+import java.nio.ShortBuffer;
+import org.junit.jupiter.api.Test;
 import uk.ac.manchester.tornado.api.types.arrays.ByteArray;
 import uk.ac.manchester.tornado.api.types.arrays.CharArray;
 import uk.ac.manchester.tornado.api.types.arrays.DoubleArray;
@@ -55,7 +179,7 @@ public class TestBuildFromByteBuffers extends TornadoTestBase {
     FloatArray floatArray = FloatArray.fromFloatBuffer(buffer);
 
     for (int i = 0; i < SIZE; i++) {
-      assertEquals(buffer.get(i), floatArray.get(i), 0.0f);
+      assertThat((double) buffer.get(i), closeTo(floatArray.get(i), 0.0f));
     }
   }
 
@@ -68,7 +192,7 @@ public class TestBuildFromByteBuffers extends TornadoTestBase {
     DoubleArray doubleArray = DoubleArray.fromDoubleBuffer(buffer);
 
     for (int i = 0; i < SIZE; i++) {
-      assertEquals(buffer.get(i), doubleArray.get(i), 0.0);
+      assertThat((double) buffer.get(i), closeTo(doubleArray.get(i), 0.0));
     }
   }
 
@@ -81,7 +205,7 @@ public class TestBuildFromByteBuffers extends TornadoTestBase {
     IntArray intArray = IntArray.fromIntBuffer(buffer);
 
     for (int i = 0; i < SIZE; i++) {
-      assertEquals(buffer.get(i), intArray.get(i));
+      assertThat(buffer.get(i), equalTo(intArray.get(i)));
     }
   }
 
@@ -94,7 +218,7 @@ public class TestBuildFromByteBuffers extends TornadoTestBase {
     LongArray longArray = LongArray.fromLongBuffer(buffer);
 
     for (int i = 0; i < SIZE; i++) {
-      assertEquals(buffer.get(i), longArray.get(i));
+      assertThat(buffer.get(i), equalTo(longArray.get(i)));
     }
   }
 
@@ -107,7 +231,7 @@ public class TestBuildFromByteBuffers extends TornadoTestBase {
     ShortArray shortArray = ShortArray.fromShortBuffer(buffer);
 
     for (int i = 0; i < SIZE; i++) {
-      assertEquals(buffer.get(i), shortArray.get(i));
+      assertThat(buffer.get(i), equalTo(shortArray.get(i)));
     }
   }
 
@@ -120,7 +244,7 @@ public class TestBuildFromByteBuffers extends TornadoTestBase {
     CharArray charArray = CharArray.fromCharBuffer(buffer);
 
     for (int i = 0; i < SIZE; i++) {
-      assertEquals(buffer.get(i), charArray.get(i));
+      assertThat(buffer.get(i), equalTo(charArray.get(i)));
     }
   }
 
@@ -133,7 +257,7 @@ public class TestBuildFromByteBuffers extends TornadoTestBase {
     ByteArray byteArray = ByteArray.fromByteBuffer(buffer);
 
     for (int i = 0; i < SIZE; i++) {
-      assertEquals(buffer.get(i), byteArray.get(i));
+      assertThat(buffer.get(i), equalTo(byteArray.get(i)));
     }
   }
 }
